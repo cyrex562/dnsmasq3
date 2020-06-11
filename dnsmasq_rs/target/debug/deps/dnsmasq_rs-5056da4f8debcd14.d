20f95a194f9bff0c3137225c4f503fd34d81c477
Josh M <cyrex562@gmail.com>
2020-06-11 12:31:49 -0400

    conversion to rust project

---
+++ dnsmasq_rs/target/debug/deps/dnsmasq_rs-5056da4f8debcd14.d
@@ -0,0 +1,5 @@
+c:\Users\cyrex\OneDrive\Documents\GitHub\dnsmasq3\dnsmasq_rs\target\debug\deps\dnsmasq_rs-5056da4f8debcd14.rmeta: src\main.rs
+
+c:\Users\cyrex\OneDrive\Documents\GitHub\dnsmasq3\dnsmasq_rs\target\debug\deps\dnsmasq_rs-5056da4f8debcd14.d: src\main.rs
+
+src\main.rs:
---
+++ dnsmasq_rs/target/debug/deps/dnsmasq_rs-f58c33b82f79bcf7.d
@@ -0,0 +1,5 @@
+c:\Users\cyrex\OneDrive\Documents\GitHub\dnsmasq3\dnsmasq_rs\target\debug\deps\dnsmasq_rs-f58c33b82f79bcf7.rmeta: src\main.rs
+
+c:\Users\cyrex\OneDrive\Documents\GitHub\dnsmasq3\dnsmasq_rs\target\debug\deps\dnsmasq_rs-f58c33b82f79bcf7.d: src\main.rs
+
+src\main.rs:
